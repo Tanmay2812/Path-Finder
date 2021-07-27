3db61c6b0fc3e01ab4f86f7dd53af62831bf4c1c
Hellomastr <mallicktanmay28@gmai.com>
2021-07-27 12:41:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import PathFindingVisualizer from './components/PathFindingVisualizer/PathFindingVisualizer';
+import { FaGithub } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <PathFindingVisualizer></PathFindingVisualizer>
+      <footer className="foot">
+        <p>&copy; PATH-FINDING-VISUALISER by TANMAY MALLICK
+          <a href="" className="social"><FaGithub size='1.3em' color="" /></a></p>
+      </footer>
+    </div >
   );
 }
 
